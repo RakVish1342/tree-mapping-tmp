53a417383aa283d63adbbc00950ae018c204abc5
RakVish1342 <rakshith.vishwanatha@gmail.com>
2020-09-03 18:26:16 -0700

    Saved rviz files

--- catkin_ws/src/my_pcl_tutorial/src/testPcl.cpp
+++ catkin_ws/src/my_pcl_tutorial/src/testPcl.cpp
@@ -22,40 +22,49 @@ main (int argc, char** argv)
     ros::Publisher pub = nh.advertise< sensor_msgs::PointCloud2 > ("pcl/pub", 10, true);
 
     std::string path = "/home/rxth/rakshith/data/work/asu/dreams/treeMapping/dataset/4_LidarTreePoinCloudData/";
-    std::string fileName = "GUY01_000.txt";
-    path += fileName;
-
-    float x, y, z;
+    // std::vector<std::string> fileNames = {"GUY01_000.txt"};
+    std::vector<std::string> fileNames = {"GUY01_000.txt", "GUY02_000.txt", "GUY03_000.txt"};
+    // std::vector<std::string> fileNames = {"IND01_054.txt", "IND02_067.txt", "IND03_088.txt"};
+    // std::vector<std::string> fileNames = {"MDD01_006.txt", "MDD02_013.txt", "MDD03_011.txt"};
+    // std::vector<std::string> fileNames = {"GUY01_000.txt", "IND01_054.txt", "MDD01_006.txt"};
 
     // pcl::PointCloud<pcl::PointXYZ> cloud; //Convert to pointer type
     // wrong since it returns a pointer. But ::Ptr type is already a pointer of its own kind. So just initialize it as an arg to the datatype
     // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = new pcl::PointCloud<pcl::PointXYZ>(); 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPcl(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointXYZ point;
-
-    int ctr = 0;
-    int tmp;
-    int maxPoints = 2000000; // Load upto 2 million points
-    std::ifstream fileIn(path);
-    while(fileIn >> x >> y >> z)
+    float x, y, z;
+    for(int i = 0; i<fileNames.size(); ++i)
     {
-        if(ctr >= maxPoints)
+
+        std::string fileName = fileNames[i];
+        std::string fullPath = path + fileName;
+
+        int ctr = 0;
+        int tmp;
+        int maxPoints = 2000000; // Load upto 2 million points per tree
+        std::ifstream fileIn(fullPath);
+        cout << "Adding Tree: " << fileName << endl;        
+        while(fileIn >> x >> y >> z)
         {
-            break;
+            if(ctr >= maxPoints)
+            {
+                break;
+            }
+
+            if( (ctr % (maxPoints/100) ) == 0)
+            {
+                cout << "ctr: " << ctr << endl;
+            }
+
+            // cout << "xyz: " << x << ", " << y << ",  " << z << endl;
+            point.x = x;
+            point.y = y;
+            point.z = z;
+            cloudPcl->push_back(point);
+
+            ctr += 1;
         }
-
-        if( (ctr % (maxPoints/1000) ) == 0)
-        {
-            cout << "ctr: " << ctr << endl;
-        }
-
-        // cout << "xyz: " << x << ", " << y << ",  " << z << endl;
-        point.x = x;
-        point.y = y;
-        point.z = z;
-        cloudPcl->push_back(point);
-
-        ctr += 1;
     }
 
     sensor_msgs::PointCloud2 cloudRos;
